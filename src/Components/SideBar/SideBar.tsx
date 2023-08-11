7df1707bdd33406aaae9630e4494ba60fed34907
Shehroz <mshehroz302@gmail.com>
2023-08-12 01:54:17 +0500

    Create logo
    create a logo of sidebar

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from '../../Assest/Images/logo-ct-dark.png'
 
 const SideBar = () => {
   return (
-    <div className='text-red-800  text-4xl bg-slate-400'>SideBar....</div>
+    <aside className='h-full w-64 bg-[#e5e9ed] mt-4 mr-0 mb-4 ml-4'>
+      <div className='h-20 px-8 py-6'>
+        <a className='flex items-center'>
+          <img className='max-h-8' src={logo} alt='main-logo' />
+          <span className='text-[#344767] text-sm font-semibold ml-2'>Soft UI Dashboard Pro</span>
+        </a>
+      </div>
+    </aside>
   )
 }
 
