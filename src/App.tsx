315dfbad8226cb90642f3a5aedcf4adfa54b5809
Shehroz <mshehroz302@gmail.com>
2023-08-12 00:43:48 +0500

    Add file

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import SideBar from './Components/SideBar/SideBar';
+import './Assest/Style/Index.css'
+function App() {
+  return (
+    <SideBar/>
+  );
+}
+
+export default App;
---
+++ src/Components/SideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='text-red-800  text-4xl bg-red-400'>SideBar</div>
+  )
+}
+
+export default SideBar
